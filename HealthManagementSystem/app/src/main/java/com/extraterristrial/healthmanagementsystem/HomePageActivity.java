c0db95288b655a57bf3fd97860528c21cb1565de
ExtraTerristrial <ExtraTerristrial>
2015-12-12 02:51:44 +0600

    PC-12 profilelist is populated frm database

--- HealthManagementSystem/app/src/main/java/com/extraterristrial/healthmanagementsystem/HomePageActivity.java
+++ HealthManagementSystem/app/src/main/java/com/extraterristrial/healthmanagementsystem/HomePageActivity.java
@@ -10,17 +10,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.extraterristrial.healthmanagementsystem.databaseschema.DatabaseManager;
 
 public class HomePageActivity extends AppCompatActivity {
     ListView profilelist;
     Toolbar toolbar;
+    ProfileListAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page_layout);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         profilelist=(ListView) findViewById(R.id.profile_list);
+
         toolbar.setTitle("Home");
         toolbar.inflateMenu(R.menu.menu_main);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
@@ -39,10 +45,18 @@ public class HomePageActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Under ConstrucTion", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        try{
+            adapter=new ProfileListAdapter(this,new DatabaseManager(this,null,null,DatabaseManager.DATABASE_VERSION).getUserList());
+            profilelist.setAdapter(adapter);
+        }catch(NullPointerException e){
+            Toast.makeText(this,"Profile List Empty",Toast.LENGTH_SHORT).show();
+        }
+
     }
     public void additional(View view)
     {
